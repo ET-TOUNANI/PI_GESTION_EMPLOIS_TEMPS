80c7699f6dd06b37ca141ba9f32f216f4137921d
Tounani Abderrahmane <74843162+ET-TOUNANI@users.noreply.github.com>
2023-03-25 15:43:21 +0100

    spring initializr

---
+++ pi/src/test/java/pi/enset/PiApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.enset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
