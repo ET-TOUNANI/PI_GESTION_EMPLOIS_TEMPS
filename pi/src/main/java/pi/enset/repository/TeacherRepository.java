b88c620831a3639d56c2f4d28e19ee0d681da89a
Tounani Abderrahmane <74843162+ET-TOUNANI@users.noreply.github.com>
2023-03-30 02:23:34 +0200

    Update TeacherRepository.java

--- pi/src/main/java/pi/enset/repository/TeacherRepository.java
+++ pi/src/main/java/pi/enset/repository/TeacherRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeacherRepository extends JpaRepository<Teacher,String> {
     Teacher findByUserId(String userId);
+
 }
