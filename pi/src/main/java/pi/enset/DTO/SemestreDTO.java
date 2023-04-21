4f65c838f49786a8b9ce3619f4fd3f29c687ae2e
bouargalne <bouargalne.hamid@gmail.com>
2023-04-21 00:36:07 +0000

    TypeSalle + Semestre Service and controller

---
+++ pi/src/main/java/pi/enset/DTO/SemestreDTO.java
@@ -0,0 +1,23 @@
+package pi.enset.DTO;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import pi.enset.entities.enums.NumeroSemester;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SemestreDTO {
+    private Long id;
+    private Date dateDebut;
+    private Date dateFin;
+    private NumeroSemester num;
+    private String anneeUniv;
+}
---
+++ pi/src/main/java/pi/enset/services/ISemestreService.java
@@ -0,0 +1,17 @@
+package pi.enset.services;
+
+import pi.enset.entities.Semestre;
+
+import java.util.List;
+
+public interface ISemestreService {
+    List<Semestre> getSemestres();
+
+    Semestre addSemestre(Semestre semestre);
+
+    String deleteSemestre(Long id);
+
+    Semestre getSemestreById(Long id);
+
+    Semestre updateSemestre(Long id, Semestre semestre);
+}
