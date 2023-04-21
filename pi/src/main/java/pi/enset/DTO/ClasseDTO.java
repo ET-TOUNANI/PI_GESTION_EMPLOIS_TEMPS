749e864a26dd70026d62e6cce50f0ae1bf051bf5
unknown <oumaimamc@gmail.com>
2023-04-21 12:33:17 +0000

    Task Madani : Classe + ElementdeModule

---
+++ pi/src/main/java/pi/enset/DTO/ClasseDTO.java
@@ -0,0 +1,12 @@
+package pi.enset.DTO.mappers;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClasseDTO {
+  
+}
