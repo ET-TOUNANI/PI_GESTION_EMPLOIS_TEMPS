be484a9c5daa86b2dbd1a484d29e560b567bedec
ajouad racha <rachaajouad52@gmail.com>
2023-04-24 01:52:15 +0100

    racha task- NonDisponibilite && Module

---
+++ pi/src/main/java/pi/enset/services/INonDisponibiliteService.java
@@ -0,0 +1,13 @@
+package pi.enset.services;
+
+import pi.enset.entities.NonDisponibilite;
+
+import java.util.List;
+
+public interface INonDisponibiliteService {
+    List<NonDisponibilite> getNonDisponibilites();
+    NonDisponibilite addNonDisponibilite(NonDisponibilite nonDisponibilite);
+    String deleteNonDisponibilite(Long id);
+    NonDisponibilite getNonDisponibiliteById(Long id);
+    NonDisponibilite updateNonDisponibilite(Long id,NonDisponibilite nonDisponibilite);
+}
---
+++ pi/src/main/java/pi/enset/services/INonDisponibiliteServiceImpl.java
@@ -0,0 +1,45 @@
+package pi.enset.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import pi.enset.entities.NonDisponibilite;
+import pi.enset.repository.NonDisponibiliteRepository;
+
+import java.util.List;
+@Service
+@AllArgsConstructor
+public class INonDisponibiliteServiceImpl implements INonDisponibiliteService {
+    private NonDisponibiliteRepository nonDisponibiliteRepository;
+    @Override
+    public List<NonDisponibilite> getNonDisponibilites() {
+        return nonDisponibiliteRepository.findAll();
+    }
+
+    @Override
+    public NonDisponibilite addNonDisponibilite(NonDisponibilite nonDisponibilite) {
+
+        return nonDisponibiliteRepository.save(nonDisponibilite);
+    }
+
+    @Override
+    public String deleteNonDisponibilite(Long id) {
+        try {
+            getNonDisponibiliteById(id);
+            nonDisponibiliteRepository.deleteById(id);
+            return "L'opération est bien effectuée";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    public NonDisponibilite getNonDisponibiliteById(Long id) {
+        return nonDisponibiliteRepository.findById(id).orElseThrow(()->new RuntimeException("Cette non Disponibilité n'existe pas."));
+    }
+
+    @Override
+    public NonDisponibilite updateNonDisponibilite(Long id, NonDisponibilite nonDisponibilite) {
+        nonDisponibilite.setId(id);
+        return nonDisponibiliteRepository.save(nonDisponibilite);
+    }
+}
