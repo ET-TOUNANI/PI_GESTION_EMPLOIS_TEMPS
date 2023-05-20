f7f19bcfbcb7e3d0a242a6be27b715d43fd4a4d0
ajouad racha <rachaajouad52@gmail.com>
2023-05-20 15:40:54 +0100

    form group

--- frontEnd/pi/src/app/components/add-new-classe/add-new-classe.component.ts
+++ frontEnd/pi/src/app/components/add-new-classe/add-new-classe.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-new-classe',
   templateUrl: './add-new-classe.component.html',
   styleUrls: ['./add-new-classe.component.css']
 })
-export class AddNewClasseComponent {
-
+export class AddNewClasseComponent implements OnInit{
+handleAddClasse() {
+throw new Error('Method not implemented.');
 }
+  newClassFormGroup!:FormGroup;
+  constructor(private fb:FormBuilder){}
+  ngOnInit(): void {
+   this.newClassFormGroup=this.fb.group({
+    libelle:this.fb.control(null,[Validators.required]),
+    nbrEleves:this.fb.control(null,[Validators.required]),
+    filiere:this.fb.control(null,[Validators.required]),
+   
+   });
+  }
+ // handleAddClasse() {}
+}
+
+  
+
+
+  
+  
--- frontEnd/pi/src/app/components/add-new-departement/add-new-departement.component.ts
+++ frontEnd/pi/src/app/components/add-new-departement/add-new-departement.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-new-departement',
   templateUrl: './add-new-departement.component.html',
   styleUrls: ['./add-new-departement.component.css']
 })
-export class AddNewDepartementComponent {
+export class AddNewDepartementComponent implements OnInit {
+handleAddDepartement() {
+
+
+}
+  newDepartementFormGroup!:FormGroup;
+  constructor(private fb:FormBuilder){
+
+  }
+  ngOnInit(): void {
+    this.newDepartementFormGroup=this.fb.group({
+      libelle:this.fb.control(null,[Validators.required]),
+      chefDepartement:this.fb.control(null,[Validators.required])
+    });
+    
+  }
+  
 
 }
